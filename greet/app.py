7d3cf43c7ce6232571086a5f47547cc49615adff
Mac Glass <macglass@Mes-MacBook-Pro.localdomain>
2019-08-14 11:41:02 -0700

    Finished greet

---
+++ greet/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    return '<html><head><title>welcome</title></head><body><h1>welcome</h1></body></html>'
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    return '<html><head><title>welcome</title></head><body><h1>welcome home</h1></body></html>'
+
+@app.route('/welcome/back')
+def welcome_back():
+    return '<html><head><title>welcome</title></head><body><h1>welcome back</h1></body></html>'
